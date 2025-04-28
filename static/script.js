10b13ebcd75c3049f72d0d80af29e937526763f0
Sam Hecht <shechtor18@gmail.com>
2025-04-28 19:43:45 -0400

    Fixed word cloud and added slider for num words

--- static/script.js
+++ static/script.js
@@ -4,32 +4,40 @@ document.getElementById("sourceForm").addEventListener("submit", function(e) {
     // Get selected folder values
     const folders = Array.from(document.querySelectorAll("input[name='folder']:checked"))
                          .map(cb => cb.value);
+    const words = d3.select("#word-range").property("value")
+    console.log(words)
 
     // Send folders to Flask via POST
     fetch("/wordcloud", {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ folders })
+        body: JSON.stringify({ folders: folders, words: words })
     })
-    .then(res => res.json())
+    .then(async function(response){
+        return JSON.parse(JSON.stringify((await response.json())));
+     })
     .then(data => renderWordCloud(data));
 });
 
 function renderWordCloud(words) {
+    // console.log(words)
     // Clear previous word cloud
     d3.select("#wordcloud").selectAll("*").remove();
 
+    fontScale = d3.scaleLinear(d3.extent(words.map(d => d[1])), [20, 92])
+
     const layout = d3.layout.cloud()
         .size([800, 500])
-        .words(words.map(d => ({ text: d[0], size: 10 + d[1] })))
-        .padding(5)
-        .rotate(() => ~~(Math.random() * 2) * 90)
-        .fontSize(d => d.size)
+        .words(words.map(d => { return { text: d[0], value: d[1] };}))
+        .padding(10)
+        .rotate(() => Math.floor((Math.random() * 2)) * 90)
+        .fontSize(d => fontScale(d.value))
         .on("end", draw);
 
     layout.start();
 
     function draw(words) {
+        console.log(words)
         d3.select("#wordcloud")
             .append("svg")
             .attr("width", 800)
